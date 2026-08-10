a13814e420b82aed94b1aba7f46ee0f78c5e646f
mr bigBallZ <erorndev@proton.me>
2026-08-11 00:45:57 +0300

    refactor(game): rename Schema.hpp to Commons.hpp and add VEngineCvar007 offset

--- src/game/Commons.hpp
+++ src/game/Commons.hpp
@@ -1,20 +1,14 @@
 #pragma once
 #include <cstdint>
 
-namespace Schema {
-  // --- C_BaseEntity ---
+namespace Commons {
   constexpr uint32_t m_pGameSceneNode = 0x330;
   constexpr uint32_t m_iHealth = 0x34C;
   constexpr uint32_t m_lifeState = 0x354;
-
-  // --- CGameSceneNode ---
   constexpr uint32_t m_vecAbsOrigin = 0xC8;
-
-  // --- CCSPlayerController ---
   constexpr uint32_t m_hPlayerPawn = 0x914;
   constexpr uint32_t m_bPawnIsAlive = 0x91C;
-
-  // --- C_BasePlayerPawn ---
   constexpr uint32_t m_pMovementServices = 0x1248;
   constexpr uint32_t m_angEyeAngles = 0x3350;
+  constexpr std::ptrdiff_t VEngineCvar007 = 0x3A44F0;
 }  // namespace Schema
